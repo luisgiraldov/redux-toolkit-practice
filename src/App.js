519874762924bdd63caa782f6b5b407f365ed5c7
Luis Giraldo <luisgiraldo@Luiss-MacBook-Pro.local>
2021-04-19 00:41:55 -0400

    Added PostsPage component to display posts and make use of redux store

--- src/App.js
+++ src/App.js
@@ -7,12 +7,14 @@ import {
 } from 'react-router-dom';
 
 import DashboardPage from './pages/DashboardPage.component';
+import PostsPage from './pages/PostsPage.component';
 
 const App = () => {
     return (
         <Router>
             <Switch>
                 <Route exact path="/" component={DashboardPage} />
+                <Route exact path="/posts" component={PostsPage} />
                 <Redirect to="/" />
             </Switch>
         </Router>
